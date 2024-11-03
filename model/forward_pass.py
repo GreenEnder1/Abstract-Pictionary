3578dcf5e3f07002fd2f91060c0c1a4f2cf256b9
JayTongue <jctung2@illinois.edu>
2024-11-03 06:41:09 -0600

    Finished forward_pass

--- model/forward_pass.py
+++ model/forward_pass.py
@@ -1,10 +1,52 @@
 import torch
+from torch import nn
 import torchvision
 from torchvision import transforms
 import matplotlib.pyplot as plt
-from typing import List # for type hinting
+from typing import List
+import json
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+class TinyVGG(nn.Module):
+  def __init__(self, input_shape: int, hidden_units: int, output_shape: int):
+      super().__init__()
+      self.conv_block_1 = nn.Sequential(
+          nn.Conv2d(in_channels=input_shape, out_channels=hidden_units, kernel_size=3, stride=1, padding=1),
+          nn.ReLU(),
+          nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units, kernel_size=3, stride=1, padding=1),
+          nn.ReLU(),
+          nn.MaxPool2d(kernel_size=2, stride=2)  # Reduces spatial dimensions
+      )
+      self.conv_block_2 = nn.Sequential(
+          nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units, kernel_size=3, stride=1, padding=1),
+          nn.ReLU(),
+          nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units, kernel_size=3, stride=1, padding=1),
+          nn.ReLU(),
+          nn.MaxPool2d(kernel_size=2, stride=2)  # Further reduces spatial dimensions
+      )
+      
+      # Use a dummy input to calculate the output size
+      dummy_input = torch.randn(1, input_shape, 64, 64)  # Adjust 64x64 to your input image size
+      with torch.no_grad():
+          output = self.conv_block_1(dummy_input)
+          output = self.conv_block_2(output)
+          flattened_output_size = output.view(-1).shape[0]
+
+      self.classifier = nn.Sequential(
+          nn.Flatten(),
+          nn.Linear(in_features=flattened_output_size, out_features=output_shape)
+      )
+
+  def forward(self, x):
+      x = self.conv_block_1(x)
+    #   print(x.shape)
+      x = self.conv_block_2(x)
+    #   print(x.shape)
+      x = self.classifier(x)
+    #   print(x.shape)
+      return x
+
 
 def pred_and_plot_image(model: torch.nn.Module,
                         image_path: str,
@@ -38,19 +80,38 @@ def pred_and_plot_image(model: torch.nn.Module,
         title = f'Pred: {target_image_pred_label} | Prob: {target_image_pred_probs.max().cpu():.3f}'
       plt.title(title)
       plt.axis(False)
+    return target_image_pred_probs
 
 
-model = torch.load('trained_models/model_0.pth', weights_only=False)
-model_state_dict = model['model_state_dict']
-class_names = model['class_names']
+def scoring_main(image):
 
-model.eval()
+  model_info = torch.load('model/trained_models/model_0.pth', weights_only=False)
+  model_state_dict = model_info['model_state_dict']
+  class_names = model_info['class_names']
 
-custom_image_transform = transforms.Compose([transforms.Resize(size=(600,600))])
+  model_inst = TinyVGG(input_shape=3,
+                    hidden_units=10,
+                    output_shape=len(class_names)).to(device)
 
-custom_image_path = '../dark-grey-background-phpf8ys7h4fgijgt-3755329406.jpg'
-pred_and_plot_image(model=model,
-                    image_path=custom_image_path,
-                    class_names=class_names,
-                    transform=custom_image_transform,
-                    device=device)
+  model_inst.load_state_dict(model_state_dict)
+  model_inst.eval()
+
+  custom_image_transform = transforms.Compose([transforms.Resize(size=(64,64))])
+
+  custom_image_path = image
+  target_image_pred_probs = pred_and_plot_image(model=model_inst, 
+                                                image_path=custom_image_path,
+                                                class_names=class_names,
+                                                transform=custom_image_transform,
+                                                device=device)
+
+  data_dict = dict(zip(class_names, target_image_pred_probs.tolist()[0]))
+  data_dict = dict(sorted(data_dict.items(), key=lambda item: item[1], reverse = True))
+  scores = json.dumps(data_dict, indent=4)
+
+  print(scores)
+
+  return scores
+
+if __name__ == '__main__':
+   scoring_main('data/test/dread/39.jpg')
